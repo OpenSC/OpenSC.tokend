57e7c07c8282d353124d42739d9e8b20cc7e8523
Mouse <mouse008@gmail.com>
2016-10-30 01:02:43 -0400

    Revert "removed unused reference to ISO7816Token"
    This reverts commit 6fd090fb6a38c9465619932c437cf43c93dd273f.

--- OpenSC/OpenSCToken.h
+++ OpenSC/OpenSCToken.h
@@ -43,7 +43,7 @@ class OpenSCSchema;
 //
 // "The" token
 //
-class OpenSCToken : public Tokend::Token, public TokenContext
+class OpenSCToken : public Tokend::ISO7816Token
 {
 	NOCOPY(OpenSCToken)
 		public:
